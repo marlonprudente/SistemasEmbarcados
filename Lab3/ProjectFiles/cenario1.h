9fc39ae85b382e96737ffac841705e7377935a48
Marlon <marlonoliveira@alunos.utfpr.edu.br>
2018-09-22 20:49:48 -0300

    cenarios 1 e 2

--- Lab3/ProjectFiles/cenario1.h
+++ Lab3/ProjectFiles/cenario1.h
@@ -1,6 +1,10 @@
 #ifndef CENARIO1_H
 #define CENARIO1_H
 
-extern unsigned char cenario1[];
+extern const unsigned long cenario1_termination;
+extern const unsigned long cenario1_start;
+extern const unsigned long cenario1_finish;
+extern const unsigned long cenario1_length;
+extern const unsigned char cenario1[];
 
-#endif
+#endif /* CENARIO1_H */
--- Lab3/ProjectFiles/cenario2.h
+++ Lab3/ProjectFiles/cenario2.h
@@ -1,6 +1,10 @@
 #ifndef CENARIO2_H
 #define CENARIO2_H
 
-extern unsigned char cenario2[];
+extern const unsigned long cenario2_termination;
+extern const unsigned long cenario2_start;
+extern const unsigned long cenario2_finish;
+extern const unsigned long cenario2_length;
+extern const unsigned char cenario2[];
 
-#endif
+#endif /* CENARIO2_H */
