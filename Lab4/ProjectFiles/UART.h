bd750fae5770c75a116514609fb6e837eb5c0f88
Marlon <marlonoliveira@alunos.utfpr.edu.br>
2018-10-27 18:31:17 -0300

    alt

--- Lab4/ProjectFiles/UART.h
+++ Lab4/ProjectFiles/UART.h
@@ -1,7 +1,7 @@
 #ifndef UART_H
 #define UART_H
 
-extern void init_UART_J();
+extern void init_UART();
 extern void UARTIntHandler(void);
 extern void printchar(char c);
 extern void UARTprintstring(char * string);
