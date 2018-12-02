14124f62d79d4f90cc494814379b764b894cb4bc
bnnvron <mateuss@alunos.utfpr.edu.br>
2018-12-02 17:59:55 -0200

    bandeira ieeeey

--- Lab5/ProjectFiles/exemploTIVA.c
+++ Lab5/ProjectFiles/exemploTIVA.c
@@ -151,7 +151,132 @@ void posicao_inicial()
 	servo_writeRot(24000);
 	osDelay(10000);
 }
-
+void desenha_circulo(void)
+{
+	int i, auxR, auxX;
+						servo_writePosY(1500);
+						osDelay(5000);
+						servo_writePosX(5000);
+						osDelay(5000);
+						servo_writeRot(28000);
+						osDelay(10000);
+					
+						servo_writePosX(9000);
+						osDelay(5000);
+						servo_writeRot(23000);
+						osDelay(5000);
+						servo_writePosY(6000);
+						osDelay(10000);
+						auxR = 23000;
+					  auxX = 9000;
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 200;
+							auxX+= 150;
+							osDelay(5000);
+						}
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 150;
+							auxX+= 150;
+							osDelay(5000);
+						}
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 150;
+							auxX+= 280;
+							osDelay(5000);
+						}
+						
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR+= 200;
+							auxX+= 160;
+							osDelay(5000);
+						}
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR+= 150;
+							auxX+= 200;
+							osDelay(5000);
+						}
+						servo_writePosY(5500);
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR+= 150;
+							auxX+= 350;
+							osDelay(5000);
+						}
+						auxX += 500;
+						servo_writePosX(auxX);
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR+= 150;
+							auxX+= 350;
+							osDelay(5000);
+						}
+						auxX += 500;
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR+= 200;
+							auxX-= 160;
+							osDelay(5000);
+						}
+						auxX += 500;
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR+= 150;
+							auxX-= 160;
+							osDelay(5000);
+						}
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR+= 150;
+							auxX-= 290;
+							osDelay(5000);
+						}
+						servo_writePosY(6500);
+						auxX-= 550;
+						servo_writePosX(auxX);
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 200;
+							auxX-= 150;
+							osDelay(5000);
+						}
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 150;
+							auxX-= 150;
+							osDelay(5000);
+						}
+						for(i = 0; i < 3; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 150;
+							auxX-= 280;
+							osDelay(5000);
+						}
+												
+						for(i = 0; i < 4; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 200;
+							auxX-= 150;
+							osDelay(5000);
+						}
+						for(i = 0; i < 4; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 150;
+							auxX-= 150;
+							osDelay(5000);
+						}
+						for(i = 0; i < 4; i++){
+							servo_writeRot(auxR);servo_writePosX(auxX);
+							auxR-= 150;
+							auxX-= 280;
+							osDelay(5000);
+						}
+						
+}
 void desenha_quadrado(void)
 {
 	servo_writePosY(8000);
@@ -159,7 +284,7 @@ void desenha_quadrado(void)
 	servo_writeRot(25000);
 	osDelay(10000);
 	servo_writePosX(7000);
-	servo_writeRot(20000);
+	servo_writeRot(19000);
 	osDelay(10000);
 	servo_writePosX(7000);
 	servo_writePosY(7000);
@@ -175,23 +300,24 @@ void desenha_quadrado(void)
 
 void desenha_losango()
 {
-	servo_writeRot(25000);
-	servo_writePosX(10800);
+	servo_writeRot(26000);
+	servo_writePosX(12000);
 	//servo_writePosY(7000);
 	osDelay(10000);	
-	servo_writePosY(9000);
-	servo_writePosX(6500);
 	servo_writeRot(22000);
+	osDelay(1000);
+	servo_writePosX(6500);
+	servo_writePosY(7500);
 	osDelay(10000);
 	servo_writeRot(19000);
 	servo_writePosX(10000);
 	osDelay(10000);	
 	servo_writeRot(23000);
-	servo_writePosX(17500);
+	servo_writePosX(20000);
 	osDelay(10000);	
-	servo_writePosY(7000);
 	servo_writeRot(26000);
-	servo_writePosX(10000);
+	servo_writePosX(10500);
+	osDelay(10000);	
 }
 void fibonacci_thread(void const *args){
 	uint32_t num1 = 0,num2 = 1,num3;
@@ -352,29 +478,13 @@ while(1){
 					break;
 					case '3':
 						UARTprintstring("3 - CIRCULO SELECIONADO (6 - p/ desenhar)\n\r");
-//						servo_writeRot(24000);
-//						servo_writePosX(11800);
-//						//servo_writePosY(7000);
-//						osDelay(10000);	
-//						servo_writePosY(9000);
-//						servo_writePosX(13000);
-//						osDelay(500);
-//						servo_writeRot(22000);
-//						osDelay(1000);
-//						servo_writeRot(19000);
-//						servo_writePosX(10000);
-//						osDelay(1000);	
-//						servo_writeRot(23000);
-//						servo_writePosX(17500);
-//						osDelay(1000);	
-//						servo_writePosY(7000);
-//						servo_writeRot(27000);
-//						servo_writePosX(10000);
+					desenha_circulo();
 						break;
 					case '4':
 						UARTprintstring("4 - BANDEIRA SELECIONADO (6 - p/ desenhar)\n\r");
 						desenha_quadrado();
 						desenha_losango();
+						desenha_circulo();
 						break;
 					case '5':
 						UARTprintstring("5 - PARANDO ANDAMENTO DO DESENHO...\n\r");
